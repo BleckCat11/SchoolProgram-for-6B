c4f35cce08a9f1400eb8380effd4f378e3b9a68a
unknown <stepangurgelt@gmail.com>
2022-11-28 19:42:39 +0200

    Iaddsomething

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function check(){
-    let input = document.getElementById("input").value
-    if (input = "123123"){
-        window.location.href = 'index.html';
-    }
+function check() {
+  let input = document.getElementById("input").value;
+  if (input == "123123") {
+    window.location.href = "createrealcourse.html";
+  }else{
+    document.getElementById("div").innerHTML = "пароль неправильний. спробуйте ще раз"
+  }
+
 }
