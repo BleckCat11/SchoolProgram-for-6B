d3b2a90f5b63ce0e0fd76fb709e39f7d170612c4
unknown <stepangurgelt@gmail.com>
2022-11-29 10:43:34 +0200

    i add something

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function check() {
   }
 
 }
+
+function check2(){
+    let allInputs = document.getElementsByName("alInputs")
+    for (i of allInputs){
+if (i.value.length == ""||i.value.length == " "){
+    document.getElementById("div1").innerHTML = "введіть справжні сначення в інпут!"
+    return
+}
+    }
+    if (allInputs[2].value == allInputs[3].value && allInputs[2].value.length > 4){
+        window.location.href = "index.html";
+    }
+}
